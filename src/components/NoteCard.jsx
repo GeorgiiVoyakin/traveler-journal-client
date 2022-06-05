ab3b8590d34a83cb25b72f98c34b3f50260c6765
Georgii Voyakin <voyaking@gmail.com>
2022-06-06 00:05:02 +0300

    Chenge alignment of the note card

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -15,10 +15,11 @@ function NoteCard(props) {
     <div className='note-card'>
       <Box boxShadow='outline' rounded='md' p={6}>
         <Flex>
-          <Box p='4' maxW='400px'>
+          <Box maxW='400px'>
             <Heading size='lg'>I'm a Heading</Heading>
             <Text>{props.text}</Text>
           </Box>
+          <Spacer />
           <Stack direction='column' spacing={4}>
             <IconButton aria-label='Call Segun' size='lg' icon={<EditIcon />} />
             <IconButton
