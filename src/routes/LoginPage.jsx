b6e8c36d7659c9c12ef4aaa03c4a85d0b4cab6f2
Georgii Voyakin <voyaking@gmail.com>
2022-06-05 16:03:32 +0300

    Apply code formatting

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -1,22 +1,30 @@
 import './LoginPage.css';
-import { Box, Input, FormControl, FormLabel, Center, Button, Stack, } from '@chakra-ui/react';
+import {
+  Box,
+  Input,
+  FormControl,
+  FormLabel,
+  Center,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
 
 export default function LoginPage() {
   return (
     <div className='login-page'>
       <Center h='100%'>
         <Box boxShadow='outline' p='6' rounded='md' bg='white'>
-      <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor='username'>Username</FormLabel>
-            <Input id='username' type='text' />
-            <FormLabel htmlFor='password'>Password</FormLabel>
-            <Input id='password' type='password' />
-          </FormControl>
-      <Button type="submit" colorScheme='green' w='100%'>
-      Sign in
-      </Button>
-      </Stack>
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel htmlFor='username'>Username</FormLabel>
+              <Input id='username' type='text' />
+              <FormLabel htmlFor='password'>Password</FormLabel>
+              <Input id='password' type='password' />
+            </FormControl>
+            <Button type='submit' colorScheme='green' w='100%'>
+              Sign in
+            </Button>
+          </Stack>
         </Box>
       </Center>
     </div>
