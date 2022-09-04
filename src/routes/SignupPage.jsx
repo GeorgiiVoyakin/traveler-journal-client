4d7632eeed09fb62bb5aefed826bfedc55e2d310
Georgii Voyakin <voyaking@gmail.com>
2022-09-04 18:31:27 +0300

    Implement signup

--- src/routes/SignupPage.jsx
+++ src/routes/SignupPage.jsx
@@ -7,39 +7,79 @@ import {
   Center,
   Button,
   Stack,
+  Text,
 } from '@chakra-ui/react';
 import { signup } from '../api/users';
+import { useState } from 'react';
 
 export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [firstPassword, setFirstPassword] = useState('');
+  const [secondPassword, setSecondPassword] = useState('');
+  const minPasswordLength = 4;
+
   return (
     <div className='signup-page'>
       <Center h='100%'>
-        <Box boxShadow='outline' p='6' rounded='md' bg='white'>
+        <Box boxShadow='outline' p='6' rounded='md' bg='white' minWidth={370}>
           <Stack spacing={4}>
             <FormControl>
               <FormLabel htmlFor='username'>Username</FormLabel>
-              <Input id='username' type='text' />
+              <Input
+                id='username'
+                type='text'
+                value={username}
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
               <FormLabel htmlFor='password'>Password</FormLabel>
-              <Input id='password' type='password' />
+              <Input
+                id='password'
+                type='password'
+                value={firstPassword}
+                onChange={(event) => {
+                  setFirstPassword(event.target.value);
+                }}
+              />
               <FormLabel htmlFor='password-confirm'>Retype password</FormLabel>
-              <Input id='password-confirm' type='password' />
+              <Input
+                id='password-confirm'
+                type='password'
+                value={secondPassword}
+                onChange={(event) => {
+                  setSecondPassword(event.target.value);
+                }}
+              />
             </FormControl>
-            <Button
-              type='submit'
-              colorScheme='green'
-              w='100%'
-              onClick={() => {
-                signup({})
-                  .then(() => {
-                    console.log('Ok');
-                  })
-                  .catch(() => {
-                    console.log('Not ok');
-                  });
-              }}
-            >
-              Sign up
-            </Button>
+            {firstPassword === secondPassword ? (
+              firstPassword.length < minPasswordLength ? (
+                <Text color={'red'}>
+                  Пароль не может быть короче {minPasswordLength} символов
+                </Text>
+              ) : username.length > 0 ? (
+                <Button
+                  type='submit'
+                  colorScheme='green'
+                  w='100%'
+                  onClick={() => {
+                    signup({ username, password: firstPassword })
+                      .then(() => {
+                        console.log('Ok');
+                      })
+                      .catch(() => {
+                        console.log('Not ok');
+                      });
+                  }}
+                >
+                  Sign up
+                </Button>
+              ) : (
+                <Text color={'red'}>Имя пользователя не может быть пустым</Text>
+              )
+            ) : (
+              <Text color={'red'}>Пароли не совпадают</Text>
+            )}
           </Stack>
         </Box>
       </Center>
