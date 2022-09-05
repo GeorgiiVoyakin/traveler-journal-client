cae27db065772cf441e738e7011947808c415e50
Georgii Voyakin <voyaking@gmail.com>
2022-09-05 19:18:13 +0300

    Store api host in separate variable

--- src/api/notes.js
+++ src/api/notes.js
@@ -1,24 +1,31 @@
+import { apiHost } from './config';
+
 export const getAllNotesForCurrentUser = async (userId) => {
-  return await fetch(
-    'https://traveler-journal-server.herokuapp.com/note/get-all-current-user',
-    {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    }
-  );
+  return await fetch(`${apiHost}/note/get-all-current-user`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  });
 };
 
 export const postNote = async (note) => {
-  return await fetch(
-    'https://traveler-journal-server.herokuapp.com/note/create',
-    {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(note),
-    }
-  );
+  return await fetch(`${apiHost}/note/create`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
+};
+
+export const deleteNote = async (id) => {
+  return await fetch(`${apiHost}/note/delete`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(id),
+  });
 };
--- src/api/users.js
+++ src/api/users.js
@@ -1,25 +1,21 @@
+import { apiHost } from './config';
+
 export const signup = async (user) => {
-  return await fetch(
-    'https://traveler-journal-server.herokuapp.com/auth/signup',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(user),
-    }
-  );
+  return await fetch(`${apiHost}/auth/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(user),
+  });
 };
 
 export const login = async (user) => {
-  return await fetch(
-    'https://traveler-journal-server.herokuapp.com/auth/login',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-      body: JSON.stringify(user),
-    }
-  );
+  return await fetch(`${apiHost}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(user),
+  });
 };
